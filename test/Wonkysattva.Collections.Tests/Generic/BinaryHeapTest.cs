b28a91040c86a87507735ed41df1112aa54161d8
Evan Roark <emroark@gmail.com>
2023-12-15 06:16:54 -0600

    more tests

--- test/Wonkysattva.Collections.Tests/Generic/BinaryHeapTest.cs
+++ test/Wonkysattva.Collections.Tests/Generic/BinaryHeapTest.cs
@@ -25,6 +25,7 @@ namespace Wonkysattva.Collections.Tests.Generic
 
             var result = binaryHeap.Reverse().ToList();
             
+            Assert.Equal(10, binaryHeap.Count);
             Assert.Equal(0, result[0]);
             Assert.Equal(1, result[1]);
             Assert.Equal(2, result[2]);
@@ -35,6 +36,39 @@ namespace Wonkysattva.Collections.Tests.Generic
             Assert.Equal(7, result[7]);
             Assert.Equal(8, result[8]);
             Assert.Equal(9, result[9]);
+            Assert.Equal(10, binaryHeap.Count);
+            Assert.Equal(9, binaryHeap.Pop());
+            Assert.Equal(8, binaryHeap.Pop());
+            Assert.Equal(7, binaryHeap.Pop());
+            Assert.Equal(6, binaryHeap.Pop());
+            Assert.Equal(5, binaryHeap.Pop());
+            Assert.Equal(4, binaryHeap.Pop());
+            Assert.Equal(3, binaryHeap.Pop());
+            Assert.Equal(2, binaryHeap.Pop());
+            Assert.Equal(1, binaryHeap.Pop());
+            Assert.Equal(0, binaryHeap.Pop());
+            Assert.Equal(0, binaryHeap.Count);
+            Assert.Throws<InvalidOperationException>(() => binaryHeap.Pop());
+            binaryHeap.Push(1);
+            binaryHeap.Push(0);
+            binaryHeap.Push(4);
+            binaryHeap.Push(2);
+            binaryHeap.Push(6);
+            binaryHeap.Push(3);
+            binaryHeap.Push(8);
+            binaryHeap.Push(5);
+            binaryHeap.Push(7);
+            binaryHeap.Push(9);
+            Assert.Equal(9, binaryHeap.Pop());
+            Assert.Equal(8, binaryHeap.Pop());
+            Assert.Equal(7, binaryHeap.Pop());
+            Assert.Equal(6, binaryHeap.Pop());
+            Assert.Equal(5, binaryHeap.Pop());
+            Assert.Equal(4, binaryHeap.Pop());
+            Assert.Equal(3, binaryHeap.Pop());
+            Assert.Equal(2, binaryHeap.Pop());
+            Assert.Equal(1, binaryHeap.Pop());
+            Assert.Equal(0, binaryHeap.Pop());
         }
     }
 }
